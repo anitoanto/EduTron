2f0250f22dccd6ad3667cc03155453d8bdbee9c5
Anito Anto <anitoperamangalam@gmai.com>
2020-10-31 22:52:27 +0530

    Cleared database.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     public function index()
     {
         return view('home', [
-            'randId' => Str::random(6)
+            'randId' => Str::random(4)
         ]);
     }
 }
